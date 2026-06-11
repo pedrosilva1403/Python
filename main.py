59eaabc1c3ff3aa4f1671b859b6579fcd307b824
pedrosilva1403 <pedrohenriquedasilva1403@gmail.com>
2026-06-11 19:30:55 -0300

    conexão com o bd

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-x = 1
-if x == 1:
-    print("x is equal to 1")    
-else:
-    print("x is not equal to 1")
+import mysql.connector
+
+# 1. Estabelecer a conexão
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="318546_Pedr",
+    database="cambio"
+)
+if conexao.is_connected():
+    print("Conexão bem-sucedida!")
+# 2. Criar o cursor para executar comandos
+cursor = conexao.cursor()
+
+# 3. Executar uma consulta SQL (Exemplo: Leitura de dados)
+cursor.execute("select * from br_bcb_taxa_cambio_taxa_cambio where moeda='USD'")
+
+# 4. Recuperar os resultados da consulta
+resultados = cursor.fetchall()
+for linha in resultados:
+    print(f"ID: {linha[0]} | Nome: {linha[1]}")
+
+# 5. Fechar os objetos de comunicação
+cursor.close()
+conexao.close()
---
+++ numpy/main.py
@@ -0,0 +1,10 @@
+# INTRODUÇÃO A NUMPY
+
+import numpy as np
+
+np.__version__
+
+# Criando um array NumPy
+arr = np.array([1, 2, 3, 4, 5])
+print("Array:", arr)
+
